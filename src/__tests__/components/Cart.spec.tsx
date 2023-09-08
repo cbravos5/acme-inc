6877299f661ab49a61baadc2b38d7d0cb1ee5a30
cbravos <cnbravos5@gmail.com>
2023-09-08 12:18:37 -0300

    test: add Cart tests

---
+++ src/__tests__/components/Cart.spec.tsx
@@ -0,0 +1,48 @@
+import { Cart } from '@/components/Header/Cart';
+import { render } from '@testing-library/react';
+import { useAtomValue } from 'jotai';
+import { LinkProps } from 'next/link';
+import React, { PropsWithChildren } from 'react';
+
+import '@testing-library/jest-dom';
+
+jest.mock(
+  'next/link',
+  () =>
+    ({ children, href }: PropsWithChildren<LinkProps>) =>
+      React.createElement('a', { href }, children)
+);
+
+jest.mock('jotai', () => ({
+  ...jest.requireActual('jotai'),
+  useAtomValue: jest.fn()
+}));
+
+describe('Cart Component', () => {
+  it('renders the component with cart count', () => {
+    (useAtomValue as jest.Mock).mockReturnValue([{ quantity: 2 }, { quantity: 3 }]);
+
+    const { getByText } = render(<Cart />);
+    const cartValue = getByText(5);
+
+    expect(cartValue).toBeInTheDocument();
+  });
+
+  it('renders the component with no cart count', () => {
+    (useAtomValue as jest.Mock).mockReturnValue([]);
+
+    const { queryByText } = render(<Cart />);
+    const cartValue = queryByText(/\d/i);
+
+    expect(cartValue).toBeNull();
+  });
+
+  it('has correct link to /checkout', () => {
+    (useAtomValue as jest.Mock).mockReturnValue([]);
+
+    const { container } = render(<Cart />);
+    const linkElement = container.querySelector('a');
+
+    expect(linkElement).toHaveAttribute('href', '/checkout');
+  });
+});
