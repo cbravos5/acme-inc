a89a4d7e7c4ac4a77e5adc5d9eaac4f265111f4a
cbravos <cnbravos5@gmail.com>
2023-09-08 12:24:51 -0300

    test: fix SessionStatus tests

--- src/__tests__/components/SessionStatus.spec.tsx
+++ src/__tests__/components/SessionStatus.spec.tsx
@@ -6,19 +6,6 @@ import { useAtom } from 'jotai';
 import { SessionStatus } from '@/components/Header/SessionStatus';
 import { fireEvent, render } from '@testing-library/react';
 
-jest.mock(
-  'next/link',
-  () =>
-    ({ children, href }: PropsWithChildren<LinkProps>) =>
-      React.createElement('a', { href }, children)
-);
-
-jest.mock('@radix-ui/react-popover', () => ({
-  Popover: ({ children }: PropsWithChildren) => children,
-  PopoverTrigger: ({ children }: PropsWithChildren) => children,
-  PopoverContent: ({ children }: PropsWithChildren) => children
-}));
-
 jest.mock('jotai', () => ({
   ...jest.requireActual('jotai'),
   useAtom: jest.fn()
@@ -45,12 +32,17 @@ describe('SessionStatus Component', () => {
     expect(signInButton).toBeInTheDocument();
   });
 
-  it('triggers sign out when clicked', () => {
+  it('triggers sign out when clicked', async () => {
     const setSessionMock = jest.fn();
     (useAtom as jest.Mock).mockReturnValue([mockecActiveUser, setSessionMock]);
 
-    const { getByText } = render(<SessionStatus />);
-    const signOutButton = getByText('Sair');
+    const { getByText, findByText } = render(<SessionStatus />);
+
+    const userSessionButton = getByText('Bem vindo,');
+
+    fireEvent.click(userSessionButton);
+
+    const signOutButton = await findByText('Sair');
 
     fireEvent.click(signOutButton);
 
